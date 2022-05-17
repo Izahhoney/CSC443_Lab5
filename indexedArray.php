1b75559ec96ed07ab82e5acf12bb54c666f3af89
Izahhoney <2020489338@student.uitm.edu.my>
2022-05-17 22:32:01 +0800

    Index_array

--- indexedArray.php
+++ indexedArray.php
@@ -9,12 +9,11 @@
 <body>
     <?php
     $month = array ('January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December');
-    $arrlength = count;
+    $arrlength = count($month);
     for ($x=0; $x < $arrlength; $x++) {
         echo $month[$x];
         echo "<br>";
     }
-
     ?>
     </body>
 </html>
