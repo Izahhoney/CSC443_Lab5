c5fb0956116c5994ea92ae7ca6d099e135cdadc2
Izahhoney <2020489338@student.uitm.edu.my>
2022-05-18 14:41:31 +0800

    indexedArray

--- indexedArray.php
+++ indexedArray.php
@@ -8,26 +8,24 @@
 </head>
 <body>
 <?php
-    $month = array ('January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September',
-    'October', 'November', 'December');
-    sort($month);
-    $i = 0;
-    for($i = 0; $i <= 11; $i++)
-    {
-        echo $month[$i]. "\n";
+    $month = array ('January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December');
+    //sort($month);//using for alphabetical order (sorting)
+    //$arrlength = count($month); //using for loop
+       // for ($x=0; $x < $arrlength; $x++) {
+          //  echo $month[$x];
+         //  echo "<br>";
+       // }
+
+       //using foreachloop
+    foreach($month as $key => $value){
+    echo "The value of key $key is: $value<br>";
+    echo "<br>";
     }
+    foreach ($month as $value){
+        echo "value: $value<BR>";
+    }
+   
     ?>
-    <?php
-    echo "<br>";
-    echo "<br>";
-    foreach ($month as $key => $val) { 
-        echo "The value of key $key is: $val<br>"; 
-        } 
-        /* Only retrieves the value for the current element and 
-        ignores the
-        key */ 
-        foreach ($month as $val) { 
-        echo "Value: $val<BR>"; }
-    ?>
+
     </body>
 </html>
