402d7f13c88650af58c1c78abbfbd457a89188e6
Izahhoney <2020489338@student.uitm.edu.my>
2022-05-18 22:31:14 +0800

    assocArray

--- assocArray.php
+++ assocArray.php
@@ -8,25 +8,14 @@
 </head>
 <body>
     <?php
-     //$month = array ('January','February','March','April','May','June','July', 'August', 'September','October', 'November','December');
+    $month = array ('January','February','March','April','May','June','July', 'August', 'September','October', 'November','December');
     $monthDays = array ('January' => 31,'February'=> 28,'March' =>31,'April' => 30,'May' => 31,'June' => 30,'July' => 31,'August' => 31,'September' => 30,'October' => 31,'November' => 30,'December' =>31);
-    //foreach ($monthDays as $item)
-    foreach($monthDays as $key => $days){
-        echo $key['January']; //$value<br>;
-        //echo "<br>";
-        }
-    print_r ($monthDays)
-   // $month = array ('January','February','March','April','May','June','July', 'August', 'September','October', 'November','December');
-    //$monthDays = array('January' => 31,'February'=> 28,'March' =>31,'April' => 30,'May' => 31,'June' => 30,'July' => 31,'August' => 31,'September' => 30,'October' => 31,'November' => 30,'December' =>31);
-   // foreach ($monthDays as $item)
-   // {
-       // if($item == 30)
-      //  {
-       //     echo "$month[$i] = $item <br>";
-      
-       // }
-       // $i++;
-   // }
+    foreach ($monthDays as $item => $day)
+    {
+        echo "$month[$i] as $day <br>";
+       $i++;
+    } echo "<br>";
+    
   ?>
 </body>
 </html>
