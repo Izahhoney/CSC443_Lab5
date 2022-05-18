eaf374ccf11f8b5369911b36e9e022deff281b19
Izahhoney <2020489338@student.uitm.edu.my>
2022-05-18 22:49:01 +0800

    assocArray

--- assocArray.php
+++ assocArray.php
@@ -7,15 +7,27 @@
     <title>Document</title>
 </head>
 <body>
-    <?php
-     $month = array ('January','February','March','April','May','June','July', 'August', 'September','October', 'November','December');
-    $monthDays = array ('January' => 31,'February'=> 28,'March' =>31,'April' => 30,'May' => 31,'June' => 30,'July' => 31,'August' => 31,'September' => 30,'October' => 31,'November' => 30,'December' =>31);
-    foreach ($monthDays as $item => $day)
-    {
-        echo "$month[$i] as $day <br>";
-       $i++;
-    } echo "<br>";
-    
-  ?>
+<?php
+$month = array ('January', 'February', 'March', 'April','May', 'June', 'July','August','September', 'October', 'November', 'December');
+$monthDays = array ('January' => 31, 'February' => 28, 'March' => 31,'April' => 30,'May' => 31, 'June' => 30, 'July' => 31, 'August' => 31,'September' => 30, 'October' => 31, 'November'=> 30, 'December'=> 31);
+$i = 0;
+foreach ($monthDays as $item => $day)
+{// Displaying month with days
+ echo "$month[$i] - $day <br>";
+ $i++;
+}echo "<br>";
+
+$month = array ('January', 'February', 'March', 'April','May', 'June', 'July','August','September', 'October', 'November', 'December');
+$monthDays = array ('January' => 31, 'February' => 28, 'March' => 31,'April' => 30,'May' => 31, 'June' => 30, 'July' => 31, 'August' => 31,'September' => 30, 'October' => 31, 'November'=> 30, 'December' => 31);
+$i = 0;
+foreach ($monthDays as $item => $day)
+{//displaying months with 30 days
+ if($item == 30)
+ {
+ echo "$month[$i] - $day <br>";
+}
+ $i++;
+}
+?>
 </body>
 </html>
