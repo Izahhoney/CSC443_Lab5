c4b630687d4d2e5f3e205feb8a228c1a389cae61
Izahhoney <2020489338@student.uitm.edu.my>
2022-05-19 00:24:58 +0800

    exercise

--- exercise.php
+++ exercise.php
@@ -11,7 +11,7 @@
     $monthDays = array ('Splorch' => 23, 'Sploo' => 28, 'Splat' => 2, 'Splatt' => 3,'Spleen' => 44, 'Splune' => 30,'Spling' => 61, 'Slendo' => 61,'Sploctember' => 31, 'Splictember' => 31,'Splanet' => 30, 'TheRest' => 22);
     echo "The shortest month is:".min($monthDays);
     //echo "</br>";
-    //print_r("Number of days in year:".array_sum($monthDays));
+    echo "Number of days in year:".array_sum($monthDays);
     //echo "</br>";
     //print_r("Minimum days month:".array_search(min($monthDays),$monthDays));
     ?>
