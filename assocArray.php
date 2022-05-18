bd8282a825b07545c568cf2f6243a5d626797960
Izahhoney <2020489338@student.uitm.edu.my>
2022-05-18 23:18:39 +0800

    AssocArray

--- assocArray.php
+++ assocArray.php
@@ -7,24 +7,36 @@
     <title>Document</title>
 </head>
 <body>
+
+
 <?php
-
-$monthDays = array ('January' => 31, 'February' => 28, 'March' => 31,'April' => 30,'May' => 31, 'June' => 30, 'July' => 31, 'August' => 31,'September' => 30, 'October' => 31, 'November'=> 30, 'December'=> 31);
-foreach ($monthDays as $key => $day)
-{
- echo "$key $day";
- echo "<br>";
-}
-
-{
-    if($item == 30)
-{
+$month = array ('January', 'February', 'March', 'April','May', 'June', 'July',
+'August','September', 'October', 'November', 'December');
+$monthDays = array ('January' => 31, 'February' => 28, 'March' => 31,
+'April' => 30,'May' => 31, 'June' => 30, 'July' => 31, 'August' =>
+31,'September' => 30, 'October' => 31, 'November'=> 30, 'December'
+=> 31)
+;$i = 0;
+foreach ($monthDays as $item)
+{//1. Displaying month with days
+ echo "$month[$i] - $item <br>";
+ $i++;
+}echo "<br>";
+$month = array ('January', 'February', 'March', 'April','May', 'June', 'July',
+'August','September', 'October', 'November', 'December');
+$monthDays = array ('January' => 31, 'February' => 28, 'March' => 31,
+'April' => 30,'May' => 31, 'June' => 30, 'July' => 31, 'August' =>
+31,'September' => 30, 'October' => 31, 'November'=> 30, 'December'
+=> 31)
+;$i = 0;
+foreach ($monthDays as $item)
+{//2. displaying months with 30 days
+ if($item == 30)
+ {
  echo "$month[$i] - $item <br>";
 }
  $i++;
 }
-?>
-
 ?>
 </body>
 </html>
