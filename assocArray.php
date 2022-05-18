7083e067733d805bf23c2075e6979952f4cc2d9d
Izahhoney <2020489338@student.uitm.edu.my>
2022-05-18 22:13:37 +0800

    assocArray

--- assocArray.php
+++ assocArray.php
@@ -13,8 +13,9 @@
     foreach ($monthDays as $item)
     {
         echo "$month[$i] = $item <br>";
-        $i++;
+       
     } echo "<br>";
+    $i++;
     print_r ($monthDays)
    // $month = array ('January','February','March','April','May','June','July', 'August', 'September','October', 'November','December');
     //$monthDays = array('January' => 31,'February'=> 28,'March' =>31,'April' => 30,'May' => 31,'June' => 30,'July' => 31,'August' => 31,'September' => 30,'October' => 31,'November' => 30,'December' =>31);
