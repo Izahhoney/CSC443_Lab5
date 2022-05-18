6581d596e6d97f4b8d312e69ca647664e29d8046
Izahhoney <2020489338@student.uitm.edu.my>
2022-05-18 14:17:31 +0800

    indexedArray

--- indexedArray.php
+++ indexedArray.php
@@ -9,12 +9,12 @@
 <body>
     <?php
     $month = array ('January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December');
-    //sort($month);//using for alphabetical order (sorting)
-    //$arrlength = count($month); //using for loop
-       // for ($x=0; $x < $arrlength; $x++) {
-        //    echo $month[$x];
-        //    echo "<br>";
-       // }
+    sort($month);//using for alphabetical order (sorting)
+    $arrlength = count($month); //using for loop
+        for ($x=0; $x < $arrlength; $x++) {
+            echo $month[$x];
+            echo "<br>";
+        }
     
 
        //using foreachloop
